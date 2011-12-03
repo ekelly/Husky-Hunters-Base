a4bad8559c2b1eff0ae928d9546efc6cbe042ea6
Roderic Morris <roderyc@gmail.com>
2011-12-03 01:13:27 -0500

    Get things working. Team creation endpoint.

--- src/huskyhunter/base/__init__.py
+++ src/huskyhunter/base/__init__.py
@@ -1,12 +1,34 @@
 import tornado.web
 import tornado.database
+import functools
 import json
 
 # (clue_number, answer, original_text, points, location, is_solved, photos)
 
+# Issues: no transactions.
+
+def valid_team(fun):
+  @functools.wraps(fun)
+  def wrapped(self, team, *args, **kwargs):
+    import pdb; pdb.set_trace()
+    if not self.db.execute_rowcount("select count(*) from teams where id = ?", team) > 0:
+      raise HTTPError(403)
+    fun(team, *args, **kwargs)
+  return wrapped
+
+def existing_clue(fun):
+  @functools.wraps(fun)
+  def wrapped(self, team, clue, *args, **kwargs):
+    if not self.db.execute_rowcount("select count(*) from clues where team = ? and clue_number = ?",
+                                    team, clue) > 0:
+      raise HTTPError(404)
+    fun(team, clue, *args, **kwargs)
+  return wrapped
+
 class BaseHandler(tornado.web.RequestHandler):
-  def __init__(self):
-    self.db = tornado.database.Connection("localhost", "HuskyHunterBase")
+  def __init__(self, *args, **kwargs):
+    super(BaseHandler, self).__init__(*args, **kwargs)
+    self.db = tornado.database.Connection("localhost", "HuskyHunterBase", user="root")
     
 class CluesHandler(BaseHandler):
   @valid_team
@@ -20,10 +42,15 @@ class PhotosHandler(BaseHandler):
   @existing_clue
   def get(self, team, clue):
     self.write(json.dumps([json.loads(row.body)["photos"] for row in
-                           self.db.iter("select * from clues where team = ? and clue = ?", team, clue)]))
+                           self.db.iter("select * from clues where team = ? and clue_number = ?", team, clue)]))
     db.close()
 
+class TeamsHandler(BaseHandler):
+  def get(self):
+    self.write("woot")
+
 application = tornado.web.Application([
-    (r"/team/([^/]+)/clues/", CluesHandler),
-    (r"/team/([^/]+)/clues/([^/]+)/photos/", PhotosHandler),
+    (r"/teams/", TeamsHandler),
+    (r"/teams/([^/]+)/clues/", CluesHandler),
+    (r"/teams/([^/]+)/clues/([^/]+)/photos/", PhotosHandler),
 ])
