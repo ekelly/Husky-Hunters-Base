93ad76800a56764346b0dd229dbdcc2140a56861
Roderic Morris <roderyc@gmail.com>
2011-12-03 01:12:23 -0500

    Fix installation.

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from distutils.core import setup
 
 setup(name='huskyhunter-base',
       version='1.0',
-      py_modules=['huskyhunter.base'],
+      packages=['huskyhunter', 'huskyhunter.base'],
       package_dir={'': 'src'})
