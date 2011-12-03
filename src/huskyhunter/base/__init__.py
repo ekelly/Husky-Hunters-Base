1916dffa59a4d6728ccea680008f908a6e44d245
Roderic Morris <roderyc@gmail.com>
2011-12-03 02:20:31 -0500

    Generate teams. Fix issues with validators. Post clues.

--- src/huskyhunter/base/__init__.py
+++ src/huskyhunter/base/__init__.py
@@ -2,26 +2,33 @@ import tornado.web
 import tornado.database
 import functools
 import json
+import uuid
 
 # (clue_number, answer, original_text, points, location, is_solved, photos)
 
 # Issues: no transactions.
 
+def decode_clue(clue_bytes):
+  return json.loads(clue_bytes)
+
+def generate_id():
+  base_id = uuid.uuid4()
+  return base_id.int % 100000000
+
 def valid_team(fun):
   @functools.wraps(fun)
   def wrapped(self, team, *args, **kwargs):
-    import pdb; pdb.set_trace()
-    if not self.db.execute_rowcount("select count(*) from teams where id = ?", team) > 0:
-      raise HTTPError(403)
-    fun(team, *args, **kwargs)
+    if not self.db.execute_rowcount("select * from teams where id = %s", team) > 0:
+      raise tornado.web.HTTPError(403)
+    fun(self, team, *args, **kwargs)
   return wrapped
 
 def existing_clue(fun):
   @functools.wraps(fun)
   def wrapped(self, team, clue, *args, **kwargs):
-    if not self.db.execute_rowcount("select count(*) from clues where team = ? and clue_number = ?",
+    if not self.db.execute_rowcount("select * from clues where team = %s and clue_number = %s",
                                     team, clue) > 0:
-      raise HTTPError(404)
+      raise tornado.web.HTTPError(404)
     fun(team, clue, *args, **kwargs)
   return wrapped
 
@@ -33,21 +40,34 @@ class BaseHandler(tornado.web.RequestHandler):
 class CluesHandler(BaseHandler):
   @valid_team
   def get(self, team):
-    self.write(json.dumps([json.loads(row.body) for row in
-                           self.db.iter("select * from clues where team = ?", team)]))
-    db.close()
+    self.write(json.dumps([decode_clue(row.body) for row in
+                           self.db.iter("select * from clues where team = %s", team)]))
+    self.db.close()
+
+  @valid_team
+  def post(self, team):
+    clues = json.loads(self.request.body)
+    args = reduce(lambda x, y: x + y, ([team, clue["clue_number"], json.dumps(clue)] for clue in clues))
+                             
+    self.db.execute("insert into clues (team, clue_number, body) values " +
+                    " ".join("(%s, %s, %s)" for clue in clues), *args)
+    self.db.close()
 
 class PhotosHandler(BaseHandler):
   @valid_team
   @existing_clue
   def get(self, team, clue):
-    self.write(json.dumps([json.loads(row.body)["photos"] for row in
-                           self.db.iter("select * from clues where team = ? and clue_number = ?", team, clue)]))
-    db.close()
+    self.write(json.dumps([decode_clue(row.body)["photos"] for row in
+                           self.db.iter("select * from clues where team = %s and clue_number = %s", team, clue)]))
+    self.db.close()
 
 class TeamsHandler(BaseHandler):
-  def get(self):
-    self.write("woot")
+  def post(self):
+    name = self.get_argument("name")
+    new_id = generate_id()
+    self.db.execute("insert into teams (id, name) values (%s, %s)", new_id, name)
+    self.write(json.dumps({"name": name, "id": new_id}))
+    self.db.close()
 
 application = tornado.web.Application([
     (r"/teams/", TeamsHandler),
