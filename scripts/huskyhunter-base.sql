aa33a94a9180992936e6ec5837b92100c487752e
Roderic Morris <roderyc@gmail.com>
2011-12-03 02:17:15 -0500

    Remove unused comments in sql.

--- scripts/huskyhunter-base.sql
+++ scripts/huskyhunter-base.sql
@@ -12,11 +12,5 @@ CREATE TABLE IF NOT EXISTS `clues` (
   `team` char(8) NOT NULL,
   `clue_number` int unsigned NOT NULL,
   `body` mediumblob NOT NULL,
-  -- `answer` text NOT NULL,
-  -- `original_text` text NOT NULL,
-  -- `points` int unsinged NOT NULL,
-  -- `location` text NOT NULL, 
-  -- `is_solved` tinyint(1) NOT NULL,
-  
   PRIMARY KEY (`team`,`clue_number`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
